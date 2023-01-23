d72af0f077e326f22157b6b24e6e3a79cad561ce
Roberto <105317145+ItzRoBeerT@users.noreply.github.com>
2023-01-23 13:47:44 +0100

    a

--- app.js
+++ app.js
@@ -8,44 +8,46 @@ const mongoClient = mongodb.MongoClient;
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'Puerto2717'
-
+app.use(express.json());
 let db;
 mongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('No se pudo conectar con la base de datos!')
     }
-   db = client.db(databaseName)
+    db = client.db(databaseName)
 })
 
 app.get('', (req, res) => {
     res.send('respuesta');
 })
 
-app.get('/create', (req, res) => {
+app.post('/create', (req, res) => {
     if (error) {
         res.send('algo ha salido mal')
     }
-   /*
-    db.collection('Contacto').insertOne({
+    let nombre = req.body.nombre
+
+    res.send(nombre);
+   /* db.collection('Contacto').insertOne({
         nombre: 'Roberto',
         edad: 20,
         telefono: 615525834,
         dni: '32100933V'
-    }),(error,result) =>{
+    }), (error, result) => {
         if (error) {
             return console.log('no se puedo añadir alumno');
         }
         console.log(result.op);
-    }*/  
+    }*/
 })
 
-app.get("/api", function(req, res)  { 
-    request(options, function(err, response, body) {  
-      var json = JSON.parse(body);
-      console.log(json); // Logging the output within the request function
-      res.json(request.json) //then returning the response.. The request.json is empty over here
+app.get("/api", function (req, res) {
+    request(options, function (err, response, body) {
+        var json = JSON.parse(body);
+        console.log(json); // Logging the output within the request function
+        res.json(request.json) //then returning the response.. The request.json is empty over here
     }); //closing the request function      
-  });
+});
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
